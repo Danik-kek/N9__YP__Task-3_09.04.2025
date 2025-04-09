f433b62b7f3e203f5a34c9c12c66d53c3507db6e
Aunt D <dzavizistur@gmail.com>
2025-04-09 13:06:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace N9__YP__Task_3_09._04._2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = "Text.txt";
+            if (!File.Exists(FilePath))
+            {
+                Console.Write("file not found");
+                return;
+            }
+            string text = File.ReadAllText(FilePath);
+            Console.WriteLine(text);
+        }
+    }
+}
